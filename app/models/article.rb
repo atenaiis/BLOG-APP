900f6f1f9d9c6c0d9905d8705ceef428319a2745
atenaiis <attennaisc@gmail.com>
2021-03-04 13:13:57 -0600

    Using Concerns

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
+    include Visible
     has_many :comments
   
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
+  end
+  
   end  
   
